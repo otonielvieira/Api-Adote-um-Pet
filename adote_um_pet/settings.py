22299fed843d833a0372b8f342b749b8d64b8d7f
otonielvieira <ottobuzios@gmail.com>
2022-06-13 06:45:12 -0300

    Atualizando configurações

--- adote_um_pet/settings.py
+++ adote_um_pet/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-99zua$wmeb*%#-md*76@)vlzy5y-p48(@8o_j(ohanucsl5a^_"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -125,5 +125,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "pythonadoteumpet@gmail.com"
-EMAIL_HOST_PASSWORD = "dkfrmdbtkoakchzc"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
